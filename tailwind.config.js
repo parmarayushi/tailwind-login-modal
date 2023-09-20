2071f89ac16c2e6ebbacd8284eee766e2466a283
parmarayushi <you@example.comaayushiparmar812@gmail.com>
2023-09-20 12:47:19 +0530

    feat: imported fonts in config and designed login-modal using tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: [ "./src/**/*.{html,ts}",],
   theme: {
     extend: {},
+    fontFamily:{
+      sans:["Mulish","sans-serif"],
+      mono:["Rokkitt","monospace"]
+    }
   },
   plugins: [],
 }
